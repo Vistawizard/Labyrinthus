de2b3574c91524b9f7bca0c1a70a8c7ed088d826
Vistawizard <rayan.baffoun2003@icloud.com>
2022-06-07 20:18:50 +0200

    animator controllers

--- Assets/My Prefabs/Scripts/enemy_master.cs
+++ Assets/My Prefabs/Scripts/enemy_master.cs
@@ -5,47 +5,70 @@ using UnityEngine.AI;
 
 public class enemy_master : MonoBehaviour
 {
-    public GameObject Player;
     public float Distance;
     public float radius = 10f;
+    public float movementSpeed;
 
     public bool isAngered;
+    public bool isDead;
 
     public NavMeshAgent _agent;
 
-    // LastKnownPos = 0f, 0f, 0f;
+    public GameObject Target;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isDead)
+        {
+            if (Target == null)
+            {
+                SearchForTarget();
+            }
+            else
+            {
+                FollowTarget();
+            }
 
-        Distance = Vector3.Distance(Player.transform.position, this.transform.position);
-
-        if(Distance <= radius)
-        {
-            isAngered = true;
-        }
-        else
-        {
-            isAngered = false;
-        }
-        if (isAngered)
-        {
-            _agent.isStopped = false;
-
-            _agent.SetDestination(Player.transform.position);
-        }
-        if(!isAngered)
-        {
-            _agent.isStopped = true;
         }
 
+    }
+
+    void SearchForTarget()
+    {
+        
+        Vector3 center = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        Collider[] hitColliders = Physics.OverlapSphere(center, 100);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].transform.tag == "Player")
+            {
+                Target = hitColliders[i].transform.gameObject;
+                Debug.Log("Found player");
+            }
+            i++;
+        }
+    }
+
+    void FollowTarget()
+    {
+        Vector3 targetPosition = Target.transform.position;
+        targetPosition.y = transform.position.y;
+        transform.LookAt(targetPosition);
+
+        float distance = Vector3.Distance(Target.transform.position, this.transform.position);
+        if (distance > 30)
+        {
+            transform.Translate(Vector3.forward * movementSpeed);
+        }
     }
 
     void OnDrawGizmosSelected()
