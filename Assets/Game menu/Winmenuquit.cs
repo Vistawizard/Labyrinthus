72ec86cb29e477f59784d9b49b41eec2a1a03437
Enguerrand Turcat <enguerrandturcat@epita.fr>
2022-06-09 03:30:48 +0200

    added end screen and quit button to menu

---
+++ Assets/Game menu/Winmenuquit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinMenu : MonoBehaviour
+{
+     public void Quitgame ()
+    {
+        Debug.Log("l");
+        Application.Quit();
+    }
+}
